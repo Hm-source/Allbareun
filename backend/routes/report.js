e5be416f9651361773c92b993730b22d88f70ac8
Hm-source <swo8852@gmail.com>
2022-06-05 00:42:30 +0900

    feat: add each nutrition score & total nutrition score in Report document

--- backend/routes/report.js
+++ backend/routes/report.js
@@ -52,19 +52,18 @@ router.get('/:id', auth, (req, res) => {
                         vitamin_C : total_vitaminC,
                         calcium : total_calcium,
                         salt : total_salt,
-                        carbon_score : setup.getDefaultCarbon(user_kcal, total_carbon),
-                        protein_score : setup.getDefaultProtein(user_kcal,total_protein),
-                        fat_score: setup.getDefaultFat(user_kcal, total_fat),
-                        vitamin_C_score : setup.getDefaultVitaminC(user_kcal, total_vitaminC),
-                        calcium_score : setup.getDefaultCalcium(user_kcal, total_calcium),
-                        salt_score : setup.getDefaultSalt(user_kcal, total_salt)
+                        carbon_score : setup.getCarbonScore(user_kcal, total_carbon),
+                        protein_score : setup.getProteinScore(user_kcal,total_protein),
+                        fat_score: setup.getFatScore(user_kcal, total_fat),
+                        vitamin_C_score : setup.getVitaminCScore(user_kcal, total_vitaminC),
+                        calcium_score : setup.getCalciumScore(user_kcal, total_calcium),
+                        salt_score : setup.getSaltScore(user_kcal, total_salt),
+                        nutrition_score: setup.getNutritionScore( setup.getCarbonScore(user_kcal, total_carbon),setup.getProteinScore(user_kcal,total_protein),setup.getFatScore(user_kcal, total_fat),setup.getVitaminCScore(user_kcal, total_vitaminC),setup.getCalciumScore(user_kcal, total_calcium),setup.getSaltScore(user_kcal, total_salt))
                 }}, (err, doc) => {
                 if(err) return res.json(err);
                 return res.json({total : total_kcal, doc});
             });
-        })
-        
-
+        });
     }).populate('food'); 
 });
 module.exports = router;
