96f0a9c17f4870492a10e1646c2e187a31a25fbe
Hm-source <swo8852@gmail.com>
2022-06-04 14:33:06 +0900

    fix : fix save err (food saving err by food name )

--- backend/Models/Food.js
+++ backend/Models/Food.js
@@ -1,8 +1,63 @@
 const mongoose = require('mongoose'); // mongoose를 선언해주고,
 
-const FoodSchema = mongoose.Schema({  // userSchema라는 이름의 schema를 작성해준다. 
+const FoodSchema = mongoose.Schema({ 
+    name : {
+        type: String
+    },
+    when : {
+        type: String
+    },
+    detail : {
+        type: String
+    },
+    once : {
+        type: Number
+    },
+    unit : {
+        type: Number
+    },
+    total_g: {
+        type: Number
+    },
+    total_mL : {
+        type: Number
+    },
+    kcal :{
+        type: Number
+    }, 
+    water_g : {
+        type: Number
+    }, 
+    protein_g : {
+        type: Number
+    },
+    fat_g : {
+        type: Number
+    }, 
+    carbon_g : {
+        type: Number
+    },
+    sugar_g : {
+        type: Number
+    }, 
+    calcium_mg :{
+        type: Number
+    }, 
+    magnecium_mg : {
+        type: Number
+    },
+    salt_mg :{
+        type: Number
+    }, 
+    zinc_mg :{
+        type: Number
+    },
+    vitaminC_mg :{
+        type: Number
+    }
 },    
 { collection : 'foods'});
+
 
 const Food = mongoose.model('Food', FoodSchema); 
 
