b2a1c3e1e554793002ea2ac86052e3737a2a3f4f
Hm-source <swo8852@gmail.com>
2022-09-24 19:07:54 +0900

    feat: add allbareun_agree

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello, Express');
+    res.sendFile(__dirname + '/views/allbareun_agree.html');
 });
 
 module.exports = router;
